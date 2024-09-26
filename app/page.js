e76eebeeb80391c33c5216e5ede4a1b4a05e5051
dihannahdi <dihannahdii@gmail.com>
2024-09-26 15:28:39 +0700

    added diferent quotes

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
