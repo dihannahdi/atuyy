86b04f0c6ee9a12400efb071e8011c736de16a04
dihannahdi <dihannahdii@gmail.com>
2024-09-30 17:52:42 +0700

    new

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Card, CardHeader, CardContent } from '@/components/ui/card';
